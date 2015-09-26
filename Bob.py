ca3f4b102d6f6d3ef387574b39694ed518f12667
Fabien Chiffre <fabien.chiffre@gmail.com>
2015-09-26 21:41:33 +0200

    Creates the base of the bot.

---
+++ Bob.py
@@ -0,0 +1,39 @@
+import sys
+import socket
+from base_client import LiacBot
+
+class BobClient(LiacBot):
+
+	def __init__(self, numClient, addrServer, portServer):
+		self.name= 'Bob'
+		self.numClient = numClient
+		self.ip = addrServer
+		self.port = portServer
+	
+
+	def on_move(self, state):
+		pass
+
+	def on_game_over(self, state):
+		pass
+
+	def start(self):
+		print "Bob is connecting to " + self.ip + ":" + self.port
+		super(self.__class__, self).start
+
+
+
+def print_help():
+	print "Bob : Beats the others bots"
+	print "Bob is a bot developed for Liac Chess"
+	print "Command line :"
+	print "	python Bob.py [player] [ip server] [port client]"
+	print "	player : -1 for black or 1 for white"
+
+if(len(sys.argv) != 4):
+	print_help()
+else:
+	bot = BobClient(sys.argv[1], sys.argv[2], sys.argv[3])
+	bot.start()
+	
+
