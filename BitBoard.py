7822d5355ce42a02927b33a1556cb7fbef303662
Fabien Chiffre <fabien.chiffre@gmail.com>
2015-10-01 20:56:01 +0200

    Generation of knights moves

--- BitBoard.py
+++ BitBoard.py
@@ -9,7 +9,12 @@ QUEEN = 'q'
 KNIGHT = 'n'
 
 notAFile = bitarray('0111111101111111011111110111111101111111011111110111111101111111')
+notBFile = bitarray('1011111110111111101111111011111110111111101111111011111110111111')
+notGFile = bitarray('1111110111111101111111011111110111111101111111011111110111111101')
 notHFile = bitarray('1111111011111110111111101111111011111110111111101111111011111110')
+
+notABFile = notAFile & notBFile
+notGHFile = notGFile & notHFile
 
 def print_bitboard(bb):
 		for i in range(7, -1, -1):
@@ -26,30 +31,32 @@ def rightshift(ba, count):
 class Move(object):
     def __init__(self, piece_type, pos_init, pos_final):
        self.piece_type = piece_type
+       self.pos_init = pos_init
+       self.pos_final = pos_final
 
-       self.pos_init = bitarray(64)
-       self.pos_init.setall(0)
-       self.pos_init[pos_init[0] + pos_init[1]*8] = 1
+       self.pos_init_bb = bitarray(64)
+       self.pos_init_bb.setall(0)
+       self.pos_init_bb[pos_init[0]*8 + pos_init[1]] = 1
 
 
-       self.pos_final = bitarray(64)
-       self.pos_final.setall(0)
-       self.pos_final[pos_final[0] + pos_final[1]*8] = 1
+       self.pos_final_bb = bitarray(64)
+       self.pos_final_bb.setall(0)
+       self.pos_final_bb[pos_final[0]*8 + pos_final[1]] = 1
 
-       self.from_to = self.pos_init ^ self.pos_final
+       self.from_to = self.pos_init_bb ^ self.pos_final_bb
 
 
     def apply(self, board, team):
     	new_board = copy.deepcopy(board)
     	new_board.pieces[team][self.piece_type] ^= self.from_to
-    	if((new_board.pieces[-team][PAWN] & self.pos_final).any()):
-    		new_board.pieces[-team][PAWN] ^= self.pos_final
+    	if((new_board.pieces[-team][PAWN] & self.pos_final_bb).any()):
+    		new_board.pieces[-team][PAWN] ^= self.pos_final_bb
 
-    	if((new_board.pieces[-team][KNIGHT] & self.pos_final).any()):
-    		new_board.pieces[-team][KNIGHT] ^= self.pos_final
+    	if((new_board.pieces[-team][KNIGHT] & self.pos_final_bb).any()):
+    		new_board.pieces[-team][KNIGHT] ^= self.pos_final_bb
 
-    	if((new_board.pieces[-team][QUEEN] & self.pos_final).any()):
-    		new_board.pieces[-team][QUEEN] ^= self.pos_final
+    	if((new_board.pieces[-team][QUEEN] & self.pos_final_bb).any()):
+    		new_board.pieces[-team][QUEEN] ^= self.pos_final_bb
     	return new_board
 
 class BitBoard(object):
@@ -78,7 +85,6 @@ class BitBoard(object):
 		self.pieces[BLACK][KNIGHT].setall(0)
 		
 		c = state['board']
-		print c
 		i=0
 		for row in xrange(7, -1, -1):
 			for col in xrange(0, 8):
@@ -94,44 +100,15 @@ class BitBoard(object):
 			bb &= bb - 1
 		return count
 
-	def generate_pawn(self, team, adv, empty):
-		moves = []
-		pawns =  bitarray(self.pieces[team][PAWN])
-		''' Loop for each pawn '''
-		while(pawns.any()):
-
-			pos_init = pawns.index(1)
-			bb_init = bitarray(64)
-			bb_init.setall(0)
-			bb_init[pos_init] = 1
-
-			# Movement to 1 forward
-			if team == WHITE:		
-				bb_final = rightshift(bb_init, 8) & empty
+	def _check_move(self, team, type_p, pos_init, bb_final, adv, moves):
+		if bb_final.any():
+			pos_final = bb_final.index(1)
+			if (adv & bb_final).any():
+				moves.insert(0, Move(type_p, (pos_init/8, pos_init%8), (pos_final/8, pos_final%8)))
 			else:
-				bb_final = leftshift(bb_init, 8) & empty
-			
-			if bb_final.any():
-				pos_final = bb_final.index(1)
-				moves.append(Move(PAWN, (pos_init%8, pos_init/8), (pos_final%8, pos_final/8)))
-			# Normal capture
-			if team == WHITE:
-				bb_final_1 = rightshift(bb_init, 7) & adv & notHFile
-				bb_final_2 = rightshift(bb_init, 9) & adv & notAFile
-			else:
-				bb_final_1 = leftshift(bb_init, 7) & adv & notHFile
-				bb_final_2 = leftshift(bb_init, 9) & adv & notAFile
+				moves.append(Move(type_p, (pos_init/8, pos_init%8), (pos_final/8, pos_final%8)))
 
-			if bb_final_1.any() :
-				pos_final = bb_final_1.index(1)
-				moves.append(Move(PAWN, (pos_init%8, pos_init/8), (pos_final%8, pos_final/8)))
 
-			if bb_final_2.any() :
-				pos_final = bb_final_2.index(1)
-				moves.append(Move(PAWN, (pos_init%8, pos_init/8), (pos_final%8, pos_final/8)))
-		
-			pawns ^= bb_init
-		return moves
 
 	def generate(self, team):
 		adv = bitarray(64)
@@ -149,9 +126,77 @@ class BitBoard(object):
 		empty = ~empty
 		
 		moves = []
-		moves.extend(self.generate_pawn(team, adv, empty))
+		self._generate_pawn(team, adv, empty, moves)
+		self._generate_knight(team, adv, empty, moves)
 
 		return moves
 
-	def _generate_knight(self, team, adv, empty):
-		pass
+	def _generate_pawn(self, team, adv, empty, moves):
+		pawns =  bitarray(self.pieces[team][PAWN])
+		''' Loop for each pawn '''
+		while(pawns.any()):
+
+			pos_init = pawns.index(1)
+			bb_init = bitarray(64)
+			bb_init.setall(0)
+			bb_init[pos_init] = 1
+
+			# Movement to 1 forward
+			if team == WHITE:		
+				bb_final = rightshift(bb_init, 8) & empty
+			else:
+				bb_final = leftshift(bb_init, 8) & empty
+			
+			self._check_move(team, PAWN, pos_init, bb_final, adv, moves)
+			
+			# Normal capture
+			if team == WHITE:
+				bb_final_1 = rightshift(bb_init, 7) & adv & notHFile
+				bb_final_2 = rightshift(bb_init, 9) & adv & notAFile
+			else:
+				bb_final_1 = leftshift(bb_init, 7) & adv & notHFile
+				bb_final_2 = leftshift(bb_init, 9) & adv & notAFile
+
+			self._check_move(team, PAWN, pos_init, bb_final_1, adv, moves)
+			self._check_move(team, PAWN, pos_init, bb_final_2, adv, moves)
+
+			pawns ^= bb_init
+
+	def _generate_knight(self, team, adv, empty, moves):
+		knights =  bitarray(self.pieces[team][KNIGHT])
+		
+		''' Loop for each pawn '''
+		while(knights.any()):
+			pos_init = knights.index(1)
+			
+			bb_init = bitarray(64)
+			bb_init.setall(0)
+			bb_init[pos_init] = 1
+
+			bb_final = rightshift(bb_init, 17) & notAFile & (empty | adv)
+			self._check_move(team, KNIGHT, pos_init, bb_final, adv, moves)
+
+			bb_final = rightshift(bb_init, 10) & notABFile & (empty | adv)
+			self._check_move(team, KNIGHT, pos_init, bb_final, adv, moves)
+
+			bb_final = leftshift(bb_init, 6) & notABFile & (empty | adv)
+			self._check_move(team, KNIGHT, pos_init, bb_final, adv, moves)
+
+			bb_final = leftshift(bb_init, 15) & notAFile & (empty | adv)
+			self._check_move(team, KNIGHT, pos_init, bb_final, adv, moves)
+
+			bb_final = rightshift(bb_init, 15) & notHFile & (empty | adv)
+			self._check_move(team, KNIGHT, pos_init, bb_final, adv, moves)
+
+			bb_final = rightshift(bb_init, 6) & notGHFile & (empty | adv) 
+			self._check_move(team, KNIGHT, pos_init, bb_final, adv, moves)
+		
+			bb_final = leftshift(bb_init, 10) & notGHFile & (empty | adv)
+			self._check_move(team, KNIGHT, pos_init, bb_final, adv, moves)
+
+			
+			bb_final = leftshift(bb_init, 17) & notHFile & (empty | adv)
+			self._check_move(team, KNIGHT, pos_init, bb_final, adv, moves)
+			
+
+			knights ^= bb_init
